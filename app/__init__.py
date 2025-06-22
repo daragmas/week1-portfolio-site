7634be3f802b50b1185475b6bede8448290f74f7
anithaamarnath <anithaamarnath04@gmail.com>
2025-06-22 09:41:25 +0530

    added router

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellows", url=os.getenv("URL"), profiles=profiles)
+
+
+@app.route('/about/anitha')
+def about_anitha():
+    anitha = next(
+        (profile for profile in profiles if profile['name'] == 'Anitha Amarnath'), None)
+    return render_template('anitha_about_page.html', title="Anitha", url=os.getenv("URL"), profile=anitha)
